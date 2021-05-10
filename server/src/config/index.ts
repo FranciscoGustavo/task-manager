240e3f9bd3a8639fe94747452c213a759221eead
Francisco Gustavo <hidalgofco520@gmail.com>
2021-05-10 14:49:31 -0500

    improve: prepare repo to upload to heroku

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,11 +1,13 @@
 import { Options } from 'sequelize';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export const db: Options = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'test',
-  password: 'test',
-  database: 'test',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 };
 
 export default {
